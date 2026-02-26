b8641db60952c31a91ebee2ff3a620805d3d520c
witorlinharess <witorlinhares@gmail.com>
2026-02-26 10:00:49 -0300

    Remove arquivos obsoletos e documentação desatualizada: exclui arquivos de checklist de deploy, segurança e troubleshooting, além de componentes não utilizados, simplificando a estrutura do projeto e eliminando informações redundantes.

--- pages/admin/dashboard/clientes/ativos.tsx
+++
@@ -1,8 +0,0 @@
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  return { redirect: { destination: '/admin/dashboard/clientes/manutencao', permanent: true } };
-};
-
-const ClientesAtivosRedirect: React.FC = () => null;
-export default ClientesAtivosRedirect;
